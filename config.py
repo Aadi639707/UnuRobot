fcf57096fabd0f13e8f9c2334b40c59a70ab4354
Aadi639707 <aadi639707@gmail.com>
2026-01-21 10:54:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,17 +7,17 @@ API_HASH = "d1bfc4df9e7a49882cf91fc9f98fc8dd"
 BOT_TOKEN = "8284596809:AAFutYvJvtxGBI0HjRm7uEO9cuTEufMRiCM"
 
 # --- DATABASE CONFIGURATION ---
-# New MongoDB URL with your username and password
 DB_URL = "mongodb+srv://aadi6397070:aadi6397@cluster55.fyd3s92.mongodb.net/?retryWrites=true&w=majority&appName=Cluster55"
 
 # --- USER & PERMISSIONS ---
 SUDO_USERS = [7876183821]
 sudoers = SUDO_USERS
 
-# --- BOT INTERNALS ---
+# --- GAME SETTINGS (Missing Variables Fix) ---
 games = {}
 player_game = {}
 timeout = 60
+minimum_players = 2  # Fixes the latest 'minimum_players' error
 sc = {}
 msgs = {}
 
